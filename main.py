cee6a8910974f934201cd757d08f3d32315c6577
SeanLeCornu <sean.cornu.16@ucl.ac.uk>
2025-03-01 11:51:54 +0400

    Pylint

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
+"""Test module for initial commit"""
 
 def print_hi(name):
     """Test function for initial push"""
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
 
 
 if __name__ == '__main__':
