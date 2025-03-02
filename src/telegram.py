b33c02f0645ddad136a446845638feb03e961c9f
SeanLeCornu <sean.cornu.16@ucl.ac.uk>
2025-03-02 11:07:59 +0400

    Pylint fixes

--- src/telegram.py
+++ src/telegram.py
@@ -3,11 +3,15 @@ Telegram Bot Module
 
 This module provides functionality to interact with the Telegram Bot API. It allows sending messages
 to a specified chat using a bot token and chat ID. The module is designed to be simple and reusable.
+
+Author: Sean Le Cornu
+Date: 2025-03-02
+Version: 1.0
 """
 
 import os
 import requests
-from src.deribit import DeribitAPI, DERIBIT_API_KEY, DERIBIT_API_SECRET, BASE_URL
+from src.deribit import DeribitAPI, DERIBIT_API_KEY, DERIBIT_API_SECRET
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 CHAT_ID = os.getenv("CHAT_ID")
@@ -17,7 +21,6 @@ def send_telegram_message(bot_token, chat_id, message):
     Sends a message to a Telegram chat using the Telegram Bot API.
 
     :param bot_token: Your Telegram bot's API token.
-    :param chat_id: The chat ID of the recipient.
     :param message: The message to send.
     """
     url = f"https://api.telegram.org/bot{bot_token}/sendMessage"
@@ -30,11 +33,12 @@ def send_telegram_message(bot_token, chat_id, message):
         response = requests.post(url, json=payload, timeout=10)
         response.raise_for_status()
     except requests.exceptions.RequestException as e:
-        raise Exception(f"Failed to send message: {e}")
+        raise RuntimeError(f"Failed to send message: {e}") from e
 
 
 deribit_client = DeribitAPI(DERIBIT_API_KEY, DERIBIT_API_SECRET)
 BTC_price = deribit_client.get_public_data("btc_usd")["result"]["index_price"]
 
 send_telegram_message(BOT_TOKEN, CHAT_ID,
-                      "Hello Sean, the price of Bitcoin this morning is: " + str(round(BTC_price,1)) )
+                      "Hello Sean, the price of Bitcoin this morning is: "
+                      + str(round(BTC_price,1)) )
